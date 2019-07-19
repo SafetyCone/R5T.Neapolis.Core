5e982c02f3ed8ecd40ff82d0f2f09a01657eba6c
MinexAutomation <minex.automation@gmail.com>
2019-07-19 12:22:18 -0400

    Added NameValueArgument extensions.

---
+++ source/R5T.Neapolis.Core/Code/Extensions/NameValueExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+namespace R5T.Neapolis
+{
+    public static class NameValueExtensions
+    {
+        public static NameValueArgument SetName(this NameValueArgument nameValue, IArgumentName name)
+        {
+            nameValue.Name = name;
+
+            return nameValue;
+        }
+
+        public static NameValueArgument SetNameShort(this NameValueArgument nameValue, string name)
+        {
+            nameValue.SetName(new ShortArgumentName(name));
+
+            return nameValue;
+        }
+
+        public static NameValueArgument SetNameFull(this NameValueArgument nameValue, string name)
+        {
+            nameValue.SetName(new FullArgumentName(name));
+
+            return nameValue;
+        }
+
+        public static NameValueArgument SetValue(this NameValueArgument nameValue, IArgumentValue value)
+        {
+            nameValue.Value = value;
+
+            return nameValue;
+        }
+
+        public static NameValueArgument SetValue(this NameValueArgument nameValue, string value)
+        {
+            nameValue.SetValue(new ArgumentValue(value));
+
+            return nameValue;
+        }
+
+        public static NameValueArgument SetValuePath(this NameValueArgument nameValue, string path)
+        {
+            nameValue.SetValue(new PathArgumentValue(path));
+
+            return nameValue;
+        }
+    }
+}
